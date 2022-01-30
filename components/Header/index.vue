0463eb610f5ae9a40f643925a690502bb132ae88
Your Name <you@example.com>
2022-01-30 14:35:38 +0600

    commiting

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="header">
-    <div class="navbarone">
+    <div class="header_nav">
       <div class="logo">
-        <h3>Booking.com</h3>
+        <h2>Booking.com</h2>
       </div>
-      <div class="navbar">
-        <ul class="menu">
+      <div class="menu">
+        <ul class='menu_ul'>
           <li><a href="">BDT</a></li>
           <li>
             <a href=""><img src="~/assets/img/Usa.png" alt="" /></a>
@@ -19,16 +18,6 @@
         </ul>
       </div>
     </div>
-    <div class="navbartwo">
-      <ul>
-        <li><a href="">Stays</a></li>
-        <li><a href="">Flights</a></li>
-        <li><a href="">Car entals</a></li>
-        <li><a href="">Attractions</a></li>
-        <li><a href="">Airport Taxis</a></li>
-      </ul>
-    </div>
-  </div>
 </template>
 <script>
 export default {
@@ -37,6 +26,8 @@ export default {
     return {
       title: "Booking.com",
       meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: "booking.com",
           name: "booking.com",
@@ -48,196 +39,102 @@ export default {
 };
 </script>
 <style>
-.navbarone {
-  width: 100%;
+*{
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+}
+
+body{
+	font-size:1.125rem;
+}
+h1{
+	font-size:3rem;
+}
+h1,h2,h3,h4,h5,h5{
+	margin:0;
+}
+a {
+      all: unset;
+ }
+ a:hover{
+	 cursor:pointer;
+ }
+
+.header_nav {
   display: flex;
-  font-family: Avenir Next, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,
-    Helvetica, Arial, sans-serif;
   background: #003580;
-  justify-content: space-between;
+  height: 60vh;
+  margin:0 auto;
+  width:100%;
 }
-.logo {
-  width: 30%;
+.logo{
   text-align: center;
+  width:100%;
+  margin:1em auto;
+
 }
-.logo h3 {
-  font-size: 1.5rem;
-  margin-top: 1em;
+
+.menu{
+  width:100%;
+  margin:0.5em auto;
 }
-.navbar {
-  width: 60%;
-  margin: 0 auto;
-  display: flex;
+.menu ul{
+  display:flex;
+  list-style:none;
+  text-decoration:none;
 }
-.navbar ul {
-  display: flex;
-  width: 100%;
-  text-decoration: none;
-  list-style: none;
-  margin-left: 20%;
+.menu li{
+  padding:0.5em ;
+  margin:0.1em;
 }
-.navbar li {
-  text-align: center;
-  padding: 0.5em 0.5em;
-  margin-right: 0.2em;
+.menu li a{
+  padding: 0.2em;
 }
-.menu a {
-  color: white;
-  text-align: center;
-}
-.navbar img {
-  border-radius: 50%;
+.menu_ul img{
+ border-radius: 50%;
   height: 30px;
 }
-.question i {
-  border-radius: 49%;
-  border: 1px solid white;
-  padding: 15px;
+li a i{
+ 
+
+  border:1px solid white;
+  padding:0.7em;
+  border-radius:50%;
 }
-.question a {
-  padding-left: 8px;
-  padding-right: 8px;
-}
-.btnBox {
-  border: 1px solid white;
-  text-align: center;
-  margin: 0.5em;
+.btnBox{
+    border: 1px solid white;
+    font-weight:bold;
 }
 .menu .btnPrime {
   font-size: 1rem;
   border: 1px solid white;
   font-weight: bold;
   background: white;
-  margin: 0.5em;
 }
-.btnPrime a {
-  color: #003580 !important;
-}
-.navbartwo{
-  width:100%;
-  background:#003580;
-  height:80vh;
-  margin:0 auto;
-}
-.navbartwo ul{
-  display:flex;
-  width:50%;
-  float:left;
-  margin-left:10%;
-  list-style:none;
-  text-decoration:none;
-  
-}
-.navbartwo li{
-  margin:1em;
-}
-.navbartwo a{
-  font-size:1rem;
+.btnPrime a{
+  color:#003580;
 }
 
-
-
-
-
-
-
-
-
-
-@media screen and (max-width: 760px) {
-  .menu a,
-  .btnPrime,
-  .btnBox {
-    font-size: 0.8rem;
+@media screen and (max-width:600px) {
+  .header_nav{
+    display:block;
+    width:100%;
   }
-  .navbar li {
-    padding: 0.3em 0.3em;
-    margin-right: 0.2em;
-  }
-}
-@media screen and (min-width: 550px) and (max-width: 650px) {
-  .menu a,
-  .btnPrime,
-  .btnBox {
-    font-size: 0.7rem;
-  }
-  .navbar li {
-    padding: 0.1em 0.1em;
-    margin-right: 0.2em;
-  }
-}
-@media screen and (min-width: 450px) and (max-width: 549px) {
-  .menu a,
-  .btnPrime,
-  .btnBox {
-    font-size: 0.6rem;
-  }
-  .btnBox a {
-    padding: 0.6em;
-  }
-  .navbar li {
-    padding: 0.1em 0.1em;
-    margin-right: 0.2em;
-  }
-  .logo h3 {
-    font-size: 1.1rem;
-  }
-  .navbar ul {
-    margin-left: 15%;
+  .menu li a{
+    font-size:0.7rem;
   }
 }
 
-@media screen and (min-width: 380px) and (max-width: 449px) {
-  .logo {
-    width: 40%;
-  }
-
-  .logo h3 {
-    font-size: 1rem;
-  }
-  .navbar {
-    width: 70%;
-  }
-  .menu a,
-  .btnPrime,
-  .btnBox {
-    font-size: 0.5rem;
-  }
-  .question {
-    padding-left: 5px;
-    padding-right: 5px;
-  }
-  .navbar li {
-    padding: 0.1em 0.1em;
-    margin-right: 0.2em;
-  }
-  .navbar ul {
-    margin-left: 10%;
+@media screen and (max-width:430px) {
+  .menu li a{
+    font-size:0.7rem;
   }
 }
-@media screen and (min-width: 200px) and (max-width: 380px) {
-  .logo {
-    width: 40%;
-  }
-
-  .logo h3 {
-    font-size: 0.8rem;
-    float: left;
-  }
-  .navbar {
-    width: 70%;
-  }
-  .menu a,
-  .btnPrime,
-  .btnBox {
-    font-size: 0.3rem;
-  }
-  .navbar li {
-    padding: 1px 1px;
-    margin-right: 0.2em;
-  }
-  .navbar ul {
-    margin-left: 5%;
+@media screen and (min-width:288px) and (max-width:366px){
+  .menu li{
+    padding:0.4em;
   }
 }
+
 </style>
